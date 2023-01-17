9d3dc68931963f0f61ba4effd1605942ff200752
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-01-17 20:10:28 +0200

    :sparkles: Add live map

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+	import { quintOut } from 'svelte/easing';
 	import { report } from '$lib/report';
 	import { formatDate, formatDistance, formatTimes } from '$lib/format';
 </script>
@@ -15,6 +18,61 @@
 	{#if !$report}
 		<p>Report not found</p>
 	{:else}
+		<section class="w-screen max-w-lg ">
+			<h2 class="text-xl text-slate-900">Live map</h2>
+			<div class="my-2 aspect-square w-full relative md:sticky md:top-4 bg-slate-100 overflow-clip">
+				<!-- Map gridlines -->
+				<div aria-hidden="true" class="absolute top-[20%] w-full border-b border-slate-500" />
+				<div aria-hidden="true" class="absolute top-[40%] w-full border-b border-slate-500" />
+				<div aria-hidden="true" class="absolute top-[60%] w-full border-b border-slate-500" />
+				<div aria-hidden="true" class="absolute top-[80%] w-full border-b border-slate-500" />
+				<div aria-hidden="true" class="absolute left-[20%] h-full border-r border-slate-500" />
+				<div aria-hidden="true" class="absolute left-[40%] h-full border-r border-slate-500" />
+				<div aria-hidden="true" class="absolute left-[60%] h-full border-r border-slate-500" />
+				<div aria-hidden="true" class="absolute left-[80%] h-full border-r border-slate-500" />
+				<!-- Danger circle -->
+				<div
+					aria-hidden="true"
+					class="absolute top-1/2 left-1/2 h-[40%] w-[40%] -translate-x-1/2 -translate-y-1/2 rounded-full bg-red-600 bg-opacity-30"
+				/>
+				<!-- Nest position -->
+				<div
+					aria-hidden="true"
+					class="absolute top-[50%] left-[50%] h-[1%] w-[1%] -translate-x-1/2 -translate-y-1/2 rounded-full bg-slate-700"
+				/>
+				<!-- Map scale -->
+				<span class="absolute font-mono left-[10%] bottom-0 -translate-x-1/2 text-slate-700">
+					100 m
+				</span>
+				<!-- Drones -->
+				{#each $report.latestCapture.drones as { serialNumber, positionY, positionX } (serialNumber)}
+					<div
+						transition:fade
+						animate:flip={{ duration: 1000, easing: quintOut }}
+						aria-label="Drone {serialNumber}"
+						class="absolute h-6 w-6 -translate-x-1/2 -translate-y-1/2 rounded-full bg-purple-500 drop-shadow-lg"
+						style="top: {positionY / 5000}%; left: {positionX / 5000}%;"
+					>
+						<span
+							class="text-xs text-slate-900 font-mono whitespace-nowrap absolute top-0 -translate-y-full left-0 -translate-x-1/4"
+						>
+							{#if Object.keys($report.recentViolations).includes(serialNumber)}
+								{@const pilot = $report.recentViolations[serialNumber].pilot}
+								{#if pilot}
+									{@const { firstName, lastName } = pilot}
+									{firstName}
+									{lastName}
+								{:else}
+									unknown
+								{/if}
+							{:else}
+								hidden
+							{/if}
+						</span>
+					</div>
+				{/each}
+			</div>
+		</section>
 		<section class="p-2 rounded-lg w-96">
 			<h2 class="text-xl text-slate-900">Recent violators</h2>
 			<ul>
