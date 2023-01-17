3c5657a9afdcaf224fa20810c00272bc3f467fa6
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-01-17 17:40:19 +0200

    :thread: Restart update task on panic, add shutdown signal

--- src/main.rs
+++ src/main.rs
@@ -36,23 +36,22 @@ async fn main() {
         .layer(cors);
 
     task::spawn(async move {
-        let mut interval = time::interval(Duration::from_secs(2));
-        let mut previous_report: Option<ParsedReport> = None;
         loop {
-            interval.tick().await;
-            let shared_state = Arc::clone(&shared_state);
-            let new_report = update_report(&previous_report).await;
-            previous_report = Some(new_report.clone());
-            let report_text = serde_json::to_string(&new_report).unwrap();
-            match shared_state.tx.send(report_text) {
-                Ok(number) => println!("Sent data to {} clients", number),
-                Err(_) => println!("No clients connected"),
-            };
+            // Spawn task for updating the report
+            let res = task::spawn(update_task(Arc::clone(&shared_state))).await;
+            match res {
+                Err(err) if err.is_panic() => println!("Update task panicked, restarting"),
+                _ => {
+                    println!("Update task failed for unknown reason...");
+                    break;
+                }
+            }
         }
     });
 
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 }
@@ -86,4 +85,32 @@ async fn websocket(stream: WebSocket, state: Arc<SharedState>) {
     }
 
     println!("Client disconnected");
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Expect tokio signal ctrl-c");
+    println!("Shutting down");
+
+    // Give the server a second to finish sending responses
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+    println!("Shutdown complete");
+}
+
+async fn update_task(shared_state: Arc<SharedState>) {
+    let mut interval = time::interval(Duration::from_secs(2));
+    let mut previous_report: Option<ParsedReport> = None;
+    loop {
+        interval.tick().await;
+        let shared_state = Arc::clone(&shared_state);
+        let new_report = update_report(&previous_report).await;
+        previous_report = Some(new_report.clone());
+        let report_text = serde_json::to_string(&new_report).unwrap();
+        match shared_state.tx.send(report_text) {
+            Ok(number) => println!("Sent data to {} clients", number),
+            Err(_) => println!("No clients connected"),
+        };
+    }
 }
